1ff57b3c267126cbe395615ed474efcc6012e7ce
Alex5 <Alex5@TOLEDO521>
2023-10-02 18:33:25 -0600

    Agregar archivos de proyecto.

---
+++ arreglos de c charp 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = new int[] { 3, 4, 5 };
+
+        Console.WriteLine("Elementoos del arreglo");
+        foreach (int numero in n)
+        {
+            Console.WriteLine(numero);
+        }
+        int suma = 0;
+        foreach (int numero in n)
+        {
+            suma += numero;
+        }
+        Console.WriteLine("suma de los elementos: " + suma);
+
+        int maximo = n[0];
+
+        foreach(int numero in n)
+        {
+            if(numero > maximo)
+            {
+                maximo = numero;
+            }
+        }
+
+        Console.WriteLine("Numero maximo: " + maximo);
+
+        Console.ReadLine();
+    }
+
+}
